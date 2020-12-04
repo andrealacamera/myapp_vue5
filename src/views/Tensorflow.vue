b37aff81d6463efcca421c09574672e7762a0078
andrealacamera <andrea.lacamera0@gmail.com>
2020-12-04 17:08:29 +0100

    open camera - not working on http

--- src/views/Tensorflow.vue
+++ src/views/Tensorflow.vue
@@ -1,9 +1,16 @@
 <template>
   <h1 class="text-3xl h-32 bg-pink-800 text-pink-100 flex justify-center items-center">{{t('detect_title')}}</h1>
-
   <div class="flex w-full flex-wrap justify-center">
     <div class="m-auto">
-
+      <div class="mt-10 flex flex-col justify-center items-center">
+        <span class="bg-red-900 text-red-300 rounded border p-4" v-if="!checkCamera">{{checkMessage}}</span>
+        <button v-if="!isStreaming" class="bg-pink-800 text-pink-100 hover:text-white border rounded py-2 px-4"  @click="openCamera">Open Camera</button>
+        <div v-else class="flex justify-between"> 
+          <button class="bg-pink-800 text-pink-100 hover:text-white border rounded py-2 px-4"  @click="screenshot">Screenshot</button>
+          <button class="bg-pink-800 text-pink-100 hover:text-white border rounded py-2 px-4"  @click="stopStreaming">Stop Streaming</button>
+        </div>
+        <video ref="videoRef" width="100" autoplay="true"></video>
+      </div>
       <div class="mt-10">
         <p>{{t('detect_img')}}:</p>
         <div class="flex justify-center">
@@ -31,11 +38,10 @@
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import { useI18n } from 'vue-i18n'
 require('@tensorflow/tfjs-backend-cpu');
-// require('@tensorflow/tfjs-backend-webgl');
-// const cocoSsd = require('@tensorflow-models/coco-ssd');
+require('@tensorflow/tfjs-backend-webgl');
 import * as cocoSsd from '@tensorflow-models/coco-ssd'
 
 export default {
@@ -43,10 +49,14 @@ export default {
 setup() {
   const { t } = useI18n();
   const imgRef = ref('');
+  const videoRef = ref('');
   const imgSrc = ref([]);
   const result = ref([]);
   const isLoading = ref(false);
+  const isStreaming = ref(false);
   const index = ref(0);
+  const checkCamera = ref(false);
+  const checkMessage = ref('');
 
   imgSrc.value = [
     require('@/assets/cat.jpg'),
@@ -70,9 +80,53 @@ setup() {
     result.value = predictions;
   }
 
-  // onMounted( () => { detect(); })
+  async function openCamera() {
+    // if (navigator.mediaDevices.getUserMedia) {
+    if ( !navigator.mediaDevices.getUserMedia ) {
+      checkMessage.value = "getUserMedia error.";
+      checkCamera.value = false;
+      return;
+    } 
+    else if (!navigator.mediaDevices.enumerateDevices) {
+      checkMessage.value="enumerateDevices error.";
+      checkCamera.value = false;
+      return;
+    } else {
+      checkCamera.value = true;
+      const devices = await navigator.mediaDevices.enumerateDevices();
+      const cams = devices.filter( (device) => device.kind === "videinput" ); //select only cameras (not mic, etc)
+      const camId = cams[0].deviceId;
+      navigator.mediaDevices.getUserMedia({video: {deviceId: {exact: camId} } })
+      .then( (stream) => {
+        isStreaming.value = true;
+        videoRef.value.srcObject = stream;
+      })
+      .catch( (err) => {
+        console.log(err);
+      })
+    }
+  }
 
-  return { t, imgRef, imgSrc, index, result, detect, isLoading }
+  function stopStreaming() {
+    const stream = videoRef.value.srcObject;
+    const tracks = stream.getTracks();
+    tracks.map(track => track.stop());
+    isStreaming.value=false;
+  }
+
+
+  function screenshot() {
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(videoRef.value, 0,0, 200, 200);
+    const data = canvas.toDataURL("image/png");
+    imgRef.value.setAttribute("src", data);
+  }
+
+
+  onMounted( openCamera() );
+
+  return { t, imgRef, videoRef, imgSrc, index, result, detect, isLoading, openCamera, checkCamera, checkMessage, isStreaming, stopStreaming, screenshot }
 }
 
 }
